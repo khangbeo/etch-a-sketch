dcc8cb1ee9efaa721385fe23d2fb2a9ed42883fe
khangbeo <83148834+khangbeo@users.noreply.github.com>
2022-12-04 23:14:28 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,50 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+// const appDiv = document.getElementById('app');
+// appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+const container = document.getElementById('container');
+const resetButton = document.querySelector('#reset');
+const clearButton = document.querySelector('#clear');
+
+resetButton.addEventListener('click', (e) => {
+  let userInput = Number.parseInt(prompt(), 10);
+  try {
+    if (userInput <= 100) {
+      reset(userInput);
+    } else if (!userInput) {
+      throw new Error("You can't have an empty input!");
+    } else {
+      throw new Error("You can't have more than 100 squares!");
+    }
+  } catch (e) {
+    alert(e);
+  }
+});
+
+function createSquares(numberOfSquares = 16) {
+  container.style.setProperty('--grid-rows', numberOfSquares);
+  container.style.setProperty('--grid-cols', numberOfSquares);
+  for (let i = 0; i < numberOfSquares**2; i++) {
+    const squareDiv = document.createElement('div');
+    squareDiv.classList.add('square');
+    container.appendChild(squareDiv);
+    sketch(squareDiv);
+  }
+}
+
+function sketch(element) {
+  element.addEventListener('mouseenter', (e) => {
+    element.classList.add('square-hover');
+  });
+}
+
+createSquares(16);
+
+function reset(userInput) {
+  container.innerHTML = '';
+  createSquares(userInput);
+}
+
