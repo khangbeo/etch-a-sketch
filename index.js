8a6356b35ca7441d31ff061bec7c9b6d36062bb4
khangbeo <83148834+khangbeo@users.noreply.github.com>
2022-12-05 05:14:21 -0800

    moved getUserInput function

--- index.js
+++ index.js
@@ -4,35 +4,30 @@ import './style.css';
 // Write Javascript code!
 // const appDiv = document.getElementById('app');
 // appDiv.innerHTML = `<h1>JS Starter</h1>`;
-let currentColor = '333333'
-let currentMode = 'black'
-let currentSize = 16
+let currentColor = '333333';
+let currentMode = 'black';
+let currentSize = 16;
 
-function setNewMode(newMode) {
-  currentMode = newMode
-}
-
-function setNewSize(newSize) {
-  currentSize = newSize
-}
+const setNewMode = (newMode) => (currentMode = newMode);
+const setNewSize = (newSize) => (currentSize = newSize);
 
 const container = document.getElementById('container');
 const resetBtn = document.querySelector('#reset');
-const blackBtn = document.querySelector('#black')
-const rainbowBtn = document.querySelector('#rainbow')
-const eraserBtn = document.querySelector('#eraser')
-const clearBtn = document.querySelector('#clear')
+const blackBtn = document.querySelector('#black');
+const rainbowBtn = document.querySelector('#rainbow');
+const eraserBtn = document.querySelector('#eraser');
+const clearBtn = document.querySelector('#clear');
 
-blackBtn.addEventListener('click', () => setNewMode('black'))
-rainbowBtn.addEventListener('click', () => setNewMode('rainbow'))
-eraserBtn.addEventListener('click', () => setNewMode('eraser'))
-clearBtn.addEventListener('click', () => clearGrid())
+blackBtn.addEventListener('click', () => setNewMode('black'));
+rainbowBtn.addEventListener('click', () => setNewMode('rainbow'));
+eraserBtn.addEventListener('click', () => setNewMode('eraser'));
+clearBtn.addEventListener('click', () => clearGrid());
 
-let mouseDown = false
-document.body.onmousedown = () => (mouseDown = true)
-document.body.onmouseup = () => (mouseDown = false)
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
 
-resetBtn.addEventListener('click', (e) => {
+const getUserInput = () => {
   let userInput = Number.parseInt(prompt(), 10);
   try {
     if (userInput <= 100) {
@@ -45,40 +40,42 @@ resetBtn.addEventListener('click', (e) => {
   } catch (e) {
     alert(e);
   }
-});
+}
+resetBtn.addEventListener('click', getUserInput);
+
 
 function createGrid(numberOfSquares = 16) {
   container.style.setProperty('--grid-rows', numberOfSquares);
   container.style.setProperty('--grid-cols', numberOfSquares);
   for (let i = 0; i < numberOfSquares ** 2; i++) {
     const squareDiv = document.createElement('div');
-    squareDiv.addEventListener('mouseover', sketch)
-    squareDiv.addEventListener('mousedown', sketch)
+    squareDiv.addEventListener('mouseover', sketch);
+    squareDiv.addEventListener('mousedown', sketch);
     container.appendChild(squareDiv);
   }
 }
 
 function sketch(e) {
-  if (e.type === 'mouseover' && !mouseDown) return
-  let randomColor = Math.floor(Math.random()*16777215).toString(16);
+  if (e.type === 'mouseover' && !mouseDown) return;
+  let randomColor = Math.floor(Math.random() * 16777215).toString(16);
 
   let modeObj = {
-    'black': `#${currentColor}`,
-    'rainbow': `#${randomColor}`,
-    'eraser': `#fefefe`
-  }
+    black: `#${currentColor}`,
+    rainbow: `#${randomColor}`,
+    eraser: `#fefefe`,
+  };
 
-  e.target.style.backgroundColor = modeObj[currentMode]
+  e.target.style.backgroundColor = modeObj[currentMode];
 }
 
 function reset(userInput) {
-  setNewSize(userInput)
-  clearGrid()
+  setNewSize(userInput);
+  clearGrid();
 }
 
 function clearGrid() {
   container.innerHTML = '';
-  createGrid(currentSize)
+  createGrid(currentSize);
 }
 
 createGrid(currentSize);
