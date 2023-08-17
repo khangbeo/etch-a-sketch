97da21d40e8b1797fcd9859e16c2e8b9f3571f82
khangbeo <83148834+khangbeo@users.noreply.github.com>
2023-08-17 14:08:17 -0700

    1 file modified

--- index.js
+++ index.js
@@ -1,58 +1,82 @@
 import './style.css';
 
-// convert to react
-// these are state variables since they change
+// State variables
 let currentColor = generateRandomColor();
 let currentMode = 'color';
 let currentSize = 16;
+let isDown = false;
 
+// DOM elements
 const container = document.querySelector('#container');
 const colorPicker = document.querySelector('#color-picker');
 const gridRange = document.querySelector('#grid-range');
 const gridRangeOutput = document.querySelector('#grid-range-output');
 const clearBtn = document.querySelector('#clear');
+const modeButtons = getModeButtons();
 
-let isDown = false;
-container.onmousedown = () => (isDown = true);
-container.onmouseup = () => (isDown = false);
+function getModeButtons() {
+  const allButtons = document.querySelectorAll('button');
+  return [...allButtons].filter((button) => button.innerHTML !== 'Clear');
+}
 
-colorPicker.addEventListener('input', (e) => setNewColor(e.target.value));
-gridRange.addEventListener('input', (e) => setNewSize(e.target.value));
-clearBtn.addEventListener('click', () => clearGrid());
+// Initialization
+function initialize() {
+  setupEventListeners();
+  loadInitialValues();
+}
 
-// grabbing all buttons then removing clear button
-const allButtons = document.querySelectorAll('button');
-const modeButtons = [...allButtons].filter(
-  (button) => button.innerHTML !== 'Clear'
-);
-// looping through each, add event listener
-modeButtons.forEach((button) => {
-  const mode = button.innerHTML.toLowerCase();
+function setupEventListeners() {
+  container.onmousedown = () => (isDown = true);
+  container.onmouseup = () => (isDown = false);
 
-  button.addEventListener('click', (e) => {
-    const clickedBtn = e.currentTarget;
-    deactivateAllBtns();
-    setNewMode(mode);
-    activateClickedBtn(clickedBtn);
+  colorPicker.addEventListener('input', (e) => updateColor(e.target.value));
+  gridRange.addEventListener('input', (e) => updateSize(e.target.value));
+  clearBtn.addEventListener('click', clearGrid);
+
+  modeButtons.forEach((button) => {
+    button.addEventListener('click', function () {
+      updateMode(this.innerHTML.toLowerCase());
+    });
   });
-});
+}
 
-const deactivateAllBtns = () =>
-  modeButtons.forEach((button) => button.classList.remove('active'));
-const activateClickedBtn = (btn) => btn.classList.add('active');
+function updateColor(newColor) {
+  currentColor = newColor;
+}
 
-const setNewColor = (newColor) => (currentColor = newColor);
-const setNewMode = (newMode) => (currentMode = newMode);
-const setNewSize = (newSize) => {
+function updateMode(newMode) {
+  deactivateAllButtons();
+  currentMode = newMode;
+  activateButton(document.querySelector(`#${newMode}`));
+}
+
+function updateSize(newSize) {
   gridRangeOutput.textContent = `${newSize} x ${newSize}`;
   currentSize = newSize;
+  recreateGrid();
+}
+
+function deactivateAllButtons() {
+  modeButtons.forEach((button) => button.classList.remove('active'));
+}
+
+function activateButton(button) {
+  button.classList.add('active');
+}
+
+function recreateGrid() {
   clearGrid();
   createGrid(currentSize);
-};
+}
+
+function clearGrid() {
+  container.innerHTML = '';
+}
 
 function createGrid(cells = 16) {
   container.style.setProperty('--grid-rows', cells);
   container.style.setProperty('--grid-cols', cells);
+
   for (let i = 0; i < cells ** 2; i++) {
     const cell = document.createElement('div');
     cell.addEventListener('mouseover', sketch);
@@ -64,11 +88,12 @@ function createGrid(cells = 16) {
 function sketch({ type, target }) {
   if (type === 'mouseover' && !isDown) return;
 
-  let modes = {
+  const modes = {
     color: currentColor,
     rainbow: generateRandomColor(),
     eraser: '',
   };
+
   target.style.backgroundColor = modes[currentMode];
 }
 
@@ -79,18 +104,12 @@ function generateRandomColor() {
     .toUpperCase()}`;
 }
 
-function clearGrid() {
-  // loop, checks for first child of container and remove until there's no children
-  container.innerHTML = '';
-  while (container.firstChild && container.removeChild(container.firstChild));
-  createGrid(currentSize);
-}
-
-function load() {
+function loadInitialValues() {
   colorPicker.value = currentColor;
   gridRange.value = currentSize;
   gridRangeOutput.innerHTML = `${currentSize} x ${currentSize}`;
   createGrid(currentSize);
 }
 
-load();
+// Load the application
+initialize();
